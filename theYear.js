489d88ec34c73cc9ff0154ea25d55d0d08463e58
Dominique Dias <70597850+dormenique@users.noreply.github.com>
2021-06-17 10:35:12 +0100

    Create theYear.js

---
+++ theYear.js
@@ -0,0 +1,21 @@
+function theYear(){
+  
+var year = document.getElementById("birthyear").value;
+var yearsOld = 2021 - year;
+
+        if (year <= 1945) {
+        generation = "Silent";
+      } else if (year <= 1964) {
+        generation = "BOOMER";
+      } else if (year <= 1980) {
+        generation = "X";
+      } else if (year <= 1996) {
+        generation = "Millennial";
+      } else {
+        generation = "Z";
+      }
+
+var resultYear = "You are" + " " + yearsOld + " " + "years old,"  + " " + "you belong to the" + " " + generation + " " + "Generation!"
+
+return(document.getElementById("resultText").innerHTML = resultYear)
+}  
